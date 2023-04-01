fa5b7be38165ea4db28fa0fe2c86e7aeaf12c7ef
Richy <richymbiamou52@gmail.com>
2023-04-01 19:45:15 +0100

    J'enleve les fonction non indispensables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -128,7 +128,7 @@ export class AppComponent {
       scene.add(pointLight);
 
 
-      // model Girl
+      // // model Girl
       // function onProgress(xhr: any) {
 
       //   if (xhr.lengthComputable) {
@@ -216,8 +216,6 @@ export class AppComponent {
 
       boxMesh[4].position.set(-16, -45, -20)
 
-      // Mettre mes thumbnail(mes boxes) en gris
-      Gris()
     }
 
     //renderer
@@ -269,19 +267,6 @@ export class AppComponent {
 
       return grayscaleTexture;
     }
-    // Function qui met les thumbnails(Mes Boxes) en gris
-    function Gris() {
-      for (let i = 0; i < boxMesh.length; i++) {
-        if (boxMesh[i].material.map) {
-          // Créer une copie de la texture d'origine
-          var originalTexture = boxMesh[i].material.map.clone();
-          var grayTexture = convertTextureToGrayscale(originalTexture);
-          if (grayTexture) {
-            boxMesh[i].material.map = grayTexture;
-          }
-        }
-      }
-    }
     // Function pour faire bouger les thumbnails
     function moveAfficheBox() {
       for (let i = 0; i < TextBox.length; i++) {
@@ -291,43 +276,39 @@ export class AppComponent {
           boxMesh[i].add(textSprite);
         }
       }
-      time -= 0.05;
-
-
-
-      boxMesh[0].position.x = -((Math.sin(time) * 13) / 15) + boxMesh[0].position.x;
-      boxMesh[0].position.y = ((Math.sin(time) / 15) + 0.2) + boxMesh[0].position.y;
-      boxMesh[0].position.z = (Math.sin(time) * 10) / 2
-
-      boxMesh[1].position.x = ((Math.sin(time) * 11) / 15) + boxMesh[1].position.x;
-      boxMesh[1].position.y = ((Math.sin(time) / 15) + 0.37) + boxMesh[1].position.y;
-      boxMesh[1].position.z = (-(Math.sin(time) * 7) / 2) + boxMesh[1].position.z / 10
-
-      boxMesh[2].position.x = -((Math.cos(time) * 5) / 15) + boxMesh[2].position.x;
-      boxMesh[2].position.y = ((Math.sin(time) / 10) + 0.4) + boxMesh[2].position.y;
-
-      boxMesh[3].position.x = ((Math.sin(time) * 25) / 40) + boxMesh[3].position.x;
-      boxMesh[3].position.y = ((Math.sin(time) / 15) + 0.2) + boxMesh[3].position.y;
-      boxMesh[3].position.z = -(Math.sin(time) * 25) / 10
-
-      boxMesh[4].position.x = -((Math.sin(time) * 13) / 15) + boxMesh[4].position.x;
-      boxMesh[4].position.y = ((Math.sin(time) / 15) + 0.23) + boxMesh[4].position.y;
-      boxMesh[4].position.z = (Math.sin(time) * 10) / 2
-      object.rotation.y = objectRock.rotation.y = object.rotation.y - 0.01
       if (boxMesh[4].position.y > 5) {
+        return
+      }else{
         time -= 0.05;
+        boxMesh[0].position.x = -((Math.sin(time) * 13) / 15) + boxMesh[0].position.x;
+        boxMesh[0].position.y = ((Math.sin(time) / 15) + 0.2) + boxMesh[0].position.y;
+        boxMesh[0].position.z = (Math.sin(time) * 10) / 2
+
+        boxMesh[1].position.x = ((Math.sin(time) * 11) / 15) + boxMesh[1].position.x;
+        boxMesh[1].position.y = ((Math.sin(time) / 15) + 0.37) + boxMesh[1].position.y;
+        boxMesh[1].position.z = (-(Math.sin(time) * 7) / 2) + boxMesh[1].position.z / 10;
+
+        boxMesh[2].position.x = -((Math.cos(time) * 5) / 15) + boxMesh[2].position.x;
+        boxMesh[2].position.y = ((Math.sin(time) / 10) + 0.4) + boxMesh[2].position.y;
+
+        boxMesh[3].position.x = ((Math.sin(time) * 25) / 40) + boxMesh[3].position.x;
+        boxMesh[3].position.y = ((Math.sin(time) / 15) + 0.2) + boxMesh[3].position.y;
+        boxMesh[3].position.z = -(Math.sin(time) * 25) / 10
+
+        boxMesh[4].position.x = -((Math.sin(time) * 13) / 15) + boxMesh[4].position.x;
+        boxMesh[4].position.y = ((Math.sin(time) / 15) + 0.23) + boxMesh[4].position.y;
+        boxMesh[4].position.z = (Math.sin(time) * 10) / 2
+        object.rotation.y = objectRock.rotation.y = object.rotation.y - 0.01
       }
+
     }
     // Fonction de l'venement qui ecoute le deplacement de la souris
     function onDocumentMouseMove(event: any) {
       moveAfficheBox()
 
-      // Calculez les coordonnées de la souris dans le système de coordonnées normalisé (-1 à 1)
       mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
       mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-      // Mettez à jour le raycaster en utilisant les coordonnées de la souris
       raycaster.setFromCamera(mouse, camera);
-      // Obtenez les objets 3D qui sont intersectés par le raycaster
       let intersects = raycaster.intersectObjects(scene.children);
 
       // Appliquer la texture en niveaux de gris à l'objet survolé
@@ -340,11 +321,10 @@ export class AppComponent {
           intersected = intersects[0].object
           intersected.userData.originalTexture = intersected.material.map
           intersected.material.map = convertTextureToGrayscale(intersected.material.map)
-
         }
       }else {
         if (intersected) {
-          intersected.userData.originalTexture = intersected.material.map
+          intersected.material.map = intersected.userData.originalTexture
         }
       }
     }
@@ -354,7 +334,6 @@ export class AppComponent {
     }
     function render() {
       renderer.render(scene, camera);
-
     }
   }
 
